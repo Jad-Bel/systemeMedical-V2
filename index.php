22257135831113ec81ecdfa4ccbbc5afbb80e12d
Jad-Bel <jadthegamer06@gmail.com>
2024-12-28 18:47:50 +0100

    include the user interface from views folder in index.php file in the root

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// Include the model files
+
+// Initialize arrays to store our patients and doctors
+
+// Function to add a patient
+
+// Function to add a doctor
+
+// Add some sample data
+
+// Handle form submissions
+
+
+// Include the view
+include 'views/index.view.php';
+
